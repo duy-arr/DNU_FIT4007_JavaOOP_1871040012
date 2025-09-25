4de93294b7bd96cf428677d6d0bc5f9ddf17ad2c
duy-arr <animelaynhs05102006@gmail.com>
2025-09-26 00:03:34 +0700

    DNU_FIT4007_JavaOOP_Ca_nhan

---
+++ src/DNU_Student.java
@@ -0,0 +1,13 @@
+public class DNU_Student {
+   public class Student {
+       private String masv;
+       private String name;
+       private int age;
+   }
+   public Student(){
+       this.masv = " ";
+       this.name = " ";
+       this.age = 0;
+   }
+
+}
